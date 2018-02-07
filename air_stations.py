4a32f849fc173e3f09b11b7248e6d0ee60fa7d56
Flowingsun007 <SunningDew@163.com>
2018-02-07 10:02:31 +0800

    Add files via upload

---
+++ air_stations.py
@@ -0,0 +1,11 @@
+import re
+import os
+import json
+import requests
+from pprint import pprint
+
+url = 'http://webresource.c-ctrip.com/code/cquery/resource/address/flight/flight_new_poi_gb2312.js?CR_2017_07_18_00_00_00'
+response = requests.get(url,verify=False)
+station = re.findall(u'([\u4e00-\u9fa5]+)\(([A-Z]+)\)', response.text)
+stations = dict(station)
+pprint(stations,indent = 4)
